58a77c7a18b42d50f7918bf73fd3bd7571b66706
聪明一只哈 <lh1357ke@163.com>
2017-04-24 08:10:31 +0800

    Update MainActivity.java
    下载测试

--- app/src/main/java/com/all/zhuzhuangtu/usegit/MainActivity.java
+++ app/src/main/java/com/all/zhuzhuangtu/usegit/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //做出修改
+        //下载测试
     }
 }
